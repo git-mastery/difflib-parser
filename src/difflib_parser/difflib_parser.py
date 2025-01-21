54a7bca4443dcc5fcd069421f1c0c502aa37111f
Woo Jia Hao <woojiahao1234@gmail.com>
2025-01-21 19:12:11 +0800

    Setup unit tests for public methods

--- src/difflib_parser/difflib_parser.py
+++ src/difflib_parser/difflib_parser.py
@@ -3,50 +3,14 @@ import difflib
 from enum import Enum
 from dataclasses import dataclass
 
+from difflib_parser.diff_line import DiffLine, DiffLineCode
+
 
 class DiffCode(Enum):
     SAME = 0
     RIGHT_ONLY = 1
     LEFT_ONLY = 2
     CHANGED = 3
-
-
-class DiffLineCode(Enum):
-    ADDED = 0
-    REMOVED = 1
-    COMMON = 2
-    MISSING = 3
-
-
-class DiffLine:
-    def __init__(self, line: str | None):
-        self.__line = line
-
-    @staticmethod
-    def parse(line: str) -> "DiffLine":
-        return DiffLine(line)
-
-    @property
-    def code(self) -> DiffLineCode | None:
-        if self.__line is None:
-            return None
-
-        match self.__line[:2]:
-            case "+ ":
-                return DiffLineCode.ADDED
-            case "- ":
-                return DiffLineCode.REMOVED
-            case "  ":
-                return DiffLineCode.COMMON
-            case "? ":
-                return DiffLineCode.MISSING
-
-    @property
-    def line(self) -> str | None:
-        if self.__line is None:
-            return None
-
-        return self.__line[2:]
 
 
 @dataclass
@@ -68,7 +32,7 @@ class Diff:
 
 # Parser inspired by https://github.com/yebrahim/difflibparser/blob/master/difflibparser.py
 class DiffParser:
-    def __init__(self, left_text, right_text):
+    def __init__(self, left_text: List[str], right_text: List[str]):
         self.__left_text = left_text
         self.__right_text = right_text
         self.__diff = list(difflib.ndiff(self.__left_text, self.__right_text))
@@ -80,6 +44,7 @@ class DiffParser:
             diff_line = DiffLine.parse(current_line)
             code = diff_line.code
             diff = Diff(code=DiffCode.SAME, line=diff_line.line)
+
             if code == DiffLineCode.ADDED:
                 diff.code = DiffCode.RIGHT_ONLY
             elif code == DiffLineCode.REMOVED:
@@ -92,6 +57,7 @@ class DiffParser:
                     diff.right_changes = change.right
                     diff.newline = change.newline
                     self.__line_no = change.skip_lines
+
             self.__line_no += 1
             yield diff
 
@@ -103,6 +69,9 @@ class DiffParser:
             for i in range(4)
         ]
 
+        [_, b, c, d] = lines
+
+        # This represents the case where both additions and removals are present in the edit
         pattern_a = [
             DiffLineCode.REMOVED,
             DiffLineCode.MISSING,
@@ -111,27 +80,29 @@ class DiffParser:
         ]
         if self.__match_pattern(lines, pattern_a):
             return DiffChange(
-                left=[i for (i, c) in enumerate(lines[1].line) if c in ["-", "^"]],
-                right=[i for (i, c) in enumerate(lines[3].line) if c in ["+", "^"]],
-                newline=lines[2].line,
+                left=[i for (i, c) in enumerate(b.line) if c in ["-", "^"]],
+                right=[i for (i, c) in enumerate(d.line) if c in ["+", "^"]],
+                newline=c.line,
                 skip_lines=3,
             )
 
+        # This represents the case where only additions are present in the edit
         pattern_b = [DiffLineCode.REMOVED, DiffLineCode.ADDED, DiffLineCode.MISSING]
         if self.__match_pattern(lines, pattern_b):
             return DiffChange(
                 left=[],
-                right=[i for (i, c) in enumerate(lines[2].line) if c in ["+", "^"]],
-                newline=lines[1].line,
+                right=[i for (i, c) in enumerate(c.line) if c in ["+", "^"]],
+                newline=b.line,
                 skip_lines=2,
             )
 
+        # This represents the case where only removals are present in the edit
         pattern_c = [DiffLineCode.REMOVED, DiffLineCode.MISSING, DiffLineCode.ADDED]
         if self.__match_pattern(lines, pattern_c):
             return DiffChange(
-                left=[i for (i, c) in enumerate(lines[1].line) for c in ["-", "^"]],
+                left=[i for (i, c) in enumerate(b.line) if c in ["-", "^"]],
                 right=[],
-                newline=lines[1].line,
+                newline=c.line,
                 skip_lines=2,
             )
 
